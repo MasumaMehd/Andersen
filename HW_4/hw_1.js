afc9a16193c0e25eca32ecbfe545a7ef1a92b89d
M Masuma <masuma.mehdiyeva@gmail.com>
2024-10-10 23:33:37 +0400

    HW_4

---
+++ HW_4/hw_1.js
@@ -0,0 +1,60 @@
+class Calculator {
+  constructor(x, y) {
+    if (!this.#isValidNumber(x) || !this.#isValidNumber(y)) {
+      throw new Error("Invalid input: x and y must be valid numbers");
+    }
+    this.x = x;
+    this.y = y;
+  }
+
+  #isValidNumber(num) {
+    return typeof num === "number" && !isNaN(num) && num !== Infinity;
+  }
+
+  setX(newX) {
+    if (!this.#isValidNumber(newX)) {
+      throw new Error("Invalid input: x must be a valid number");
+    }
+    this.x = newX;
+  }
+
+  setY(newY) {
+    if (!this.#isValidNumber(newY)) {
+      throw new Error("Invalid input: y must be a valid number");
+    }
+    this.y = newY;
+  }
+
+  logSum() {
+    return this.x + this.y;
+  }
+
+  logSub() {
+    return this.x - this.y;
+  }
+
+  logMul() {
+    return this.x * this.y;
+  }
+
+  logDiv() {
+    if (this.y === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+    return this.x / this.y;
+  }
+}
+
+const calculator = new Calculator(10, 5);
+
+console.log(calculator.logSum());
+console.log(calculator.logSub());
+console.log(calculator.logMul());
+console.log(calculator.logDiv());
+
+calculator.setX(20);
+calculator.setY(4);
+console.log(calculator.logSum());
+
+const logSumRef = calculator.logSum.bind(calculator);
+console.log(logSumRef());
