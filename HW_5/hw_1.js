45d173e61c7ec8057edd3ac2e5207731fd990d75
M Masuma <masuma.mehdiyeva@gmail.com>
2024-10-15 19:31:32 +0400

    HW_5 finished

--- HW_5/hw_1.js
+++ HW_5/hw_1.js
@@ -1,10 +1,6 @@
 class Stack {
   constructor(maxSize = 10) {
-    if (
-      typeof maxSize !== number ||
-      maxSize <= 0 ||
-      !Number.isInteger(maxSize)
-    ) {
+    if (typeof maxSize !== 'number' || maxSize <= 0 || !Number.isInteger(maxSize)) {
       throw new Error("Invalid maximum stack size.");
     }
 
@@ -13,7 +9,7 @@ class Stack {
   }
 
   push(elem) {
-    if (this.storage.length <= this.maxSize) {
+    if (this.storage.length >= this.maxSize) {
       throw new Error("Stack is full.");
     }
     this.storage.push(elem);
@@ -21,17 +17,12 @@ class Stack {
 
   pop() {
     if (this.isEmpty()) {
-      throw new Error("The stack is empty");
+      throw new Error("The stack is empty.");
     }
     return this.storage.pop();
   }
 
   peek() {
-    // if (this.isEmpty()) {
-    //   return null;
-    // }
-    // return this.storage[this.storage.length - 1];
-
     return this.isEmpty() ? null : this.storage[this.storage.length - 1];
   }
 
@@ -42,4 +33,25 @@ class Stack {
   toArray() {
     return [...this.storage];
   }
+
+  static fromIterable(iterable) {
+    if (iterable == null || typeof iterable[Symbol.iterator] !== 'function') {
+      throw new Error("Entity is not iterable.");
+    }
+
+    const stack = new Stack(iterable.length);
+    for (const item of iterable) {
+      stack.push(item);
+    }
+    return stack;
+  }
 }
+
+const myStack = new Stack(5);
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek()); 
+console.log(myStack.toArray());
+
+const fromArray = Stack.fromIterable([1, 2, 3, 4]);
+console.log(fromArray.toArray());
